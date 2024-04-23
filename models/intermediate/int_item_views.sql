603fec20c8f4f3ba24894a1036ecf6c83ae691f5
Jinyu Xu <119010357@link.cuhk.edu.cn>
2024-04-23 13:58:18 +0000

    Hi item_view int change

--- models/intermediate/int_item_views.sql
+++ models/intermediate/int_item_views.sql
@@ -1,6 +1,8 @@
 select 
     item_name,
     count(distinct item_view_ts) as time_of_item_view,
-    sum(case when add_to_cart_quantity>0 then 1 else 0 end) as time_of_cart_add
+    sum(case when add_to_cart_quantity>0 then 1 else 0 end) as time_of_cart_add,
+    round(time_of_cart_add/time_of_item_view,3) as view2cart_conversion_rate
 from {{ ref('base_snowflake_web__item_views') }}
 group by item_name
+order by view2cart_conversion_rate desc
